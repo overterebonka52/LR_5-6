f42b221fd35c3b1e99cbcc40db5b397507b192f1
илюха сакура <144677654+overterebonka52@users.noreply.github.com>
2023-11-23 04:00:33 +0300

    Add files via upload

---
+++ block1z1.pas
@@ -0,0 +1,19 @@
+﻿program block1z1;
+const Rm = 20; // размер массива
+var 
+  a: array [1..Rm] of integer; // создаем массив
+  n: integer; 
+  begin
+    writeln('Заполните массив 20 эллементами: ');
+    for n := 1 to Rm do // переменная n принимает значение от 1 до 20
+      read(a[n]);
+    for n := 1 to Rm do // обходим все элементы массива еще раз
+      begin
+      if a[n] > 0 then // проверка условия
+      a[n] := 0
+    else
+      a[n] := sqr(a[n]);
+    end;
+    for n := 1 to Rm do // цикл для вывода измененных элементов массива
+    write(a[n], ' ')
+  end.
---
+++ block1z4.pas
@@ -0,0 +1,33 @@
+﻿program block1z4;
+const
+  Rm = 30; // размер массива
+
+var
+  i, x: integer;
+  a: array[1..Rm] of integer; 
+  b: array of integer; 
+
+begin
+  randomize; 
+  SetLength(b, 0); 
+
+  for i := 1 to Rm do 
+  begin
+    x := random(167) - 99; 
+    a[i] := x; 
+
+    if x mod 2 = 0 then
+    begin
+      SetLength(b, Length(b) + 1); 
+      b[High(b)] := x; 
+    end;
+  end;
+
+  for i := 1 to Rm do 
+    write(a[i], ' ');
+
+  writeln; 
+
+  for i := 0 to High(b) do 
+    write(b[i], ' ');
+end.
